b5b834e1ee5a2d04b2756a6bbea34bb9c6e93105
nrbns357 <gusgh357@gmail.com>
2022-11-01 11:04:17 +0900

    Add: 상벌점 디테일 통신 로직 구현

--- src/components/MyInfo/PointDetail/Dormitory/Dormitory.style.ts
+++ src/components/MyInfo/PointDetail/Dormitory/Dormitory.style.ts
@@ -29,15 +29,34 @@ export const BadPoints = styled.div<{ isColor: boolean }>`
     ${({ isColor }) => (!isColor ? `#fff` : `${palette.main};`)};
 `;
 
-// export const PointDetailContainer = styled.div``;
+export const PointReasonDetailWrap = styled.div`
+  display: flex;
+  justify-content: center;
+  border: 1px solid ${palette.gray[300]};
+  border-top: 0px;
+`;
 
-// export const PointDetailContainer = styled.div``;
+export const ReasonDetail = styled.div`
+  margin: 15px 0;
+  text-align: center;
+`;
 
-// export const PointDetailContainer = styled.div``;
+export const Divide = styled.div`
+  margin: 8px 0;
+  border: 1px solid ${palette.gray[300]};
+`;
 
-// export const PointDetailContainer = styled.div``;
+// export const Date = styled.div`
+//   width: 20%;
+//   text-align: center;
+//   border-right: 1px solid ${palette.gray[400]};
+// `;
 
-// export const PointDetailContainer = styled.div``;
+// export const TeacherIdx = styled.div`
+//   width: 15%;
+//   text-align: center;
+// `;
+
 // export const PointDetailContainer = styled.div``;
 // export const PointDetailContainer = styled.div``;
 // export const PointDetailContainer = styled.div``;
--- src/components/MyInfo/PointDetail/Dormitory/Dormitory.tsx
+++ src/components/MyInfo/PointDetail/Dormitory/Dormitory.tsx
@@ -1,24 +1,104 @@
 import * as S from "./Dormitory.style";
 import { useState } from "react";
+import usePointReason from "../../../../hooks/myPoint/usePointReason";
 
 const Dormitory = () => {
 
   const [isPoint, setIsPoint] = useState<boolean>(false);
+  const { getPointDetail } = usePointReason();
 
   return (
     <S.DormitoryPointContainer>
 
       <S.PointHeaderContainer>
-        <S.RewardPoints onClick={() => setIsPoint(false)} isColor={isPoint} >상점</S.RewardPoints>
+        <S.RewardPoints onClick={() => {
+          setIsPoint(false);
+          // getPointDetail();
+        }} isColor={isPoint} >상점</S.RewardPoints>
         <S.BadPoints onClick={() => setIsPoint(true)} isColor={isPoint}>벌점</S.BadPoints>
       </S.PointHeaderContainer>
 
-      <div>
-        <div>score</div>
-        <div>reason</div>
-        <div>date</div>
-        <div>th</div>
-      </div>
+      <S.PointReasonDetailWrap>
+        <S.ReasonDetail style={{ width: "25%" }}>1점</S.ReasonDetail>
+        <S.Divide />
+        <S.ReasonDetail style={{ width: "50%" }}>기숙사 생활을 위한 봉사활동</S.ReasonDetail>
+        <S.Divide />
+        <S.ReasonDetail style={{ width: "20%" }}>2022-09-21</S.ReasonDetail>
+        <S.Divide />
+        <S.ReasonDetail style={{ width: "10%" }}>윤성원</S.ReasonDetail>
+      </S.PointReasonDetailWrap>
+      <S.PointReasonDetailWrap>
+        <S.ReasonDetail style={{ width: "25%" }}>1점</S.ReasonDetail>
+        <S.Divide />
+        <S.ReasonDetail style={{ width: "50%" }}>기숙사 생활을 위한 봉사활동</S.ReasonDetail>
+        <S.Divide />
+        <S.ReasonDetail style={{ width: "20%" }}>2022-09-21</S.ReasonDetail>
+        <S.Divide />
+        <S.ReasonDetail style={{ width: "10%" }}>윤성원</S.ReasonDetail>
+      </S.PointReasonDetailWrap>
+      <S.PointReasonDetailWrap>
+        <S.ReasonDetail style={{ width: "25%" }}>1점</S.ReasonDetail>
+        <S.Divide />
+        <S.ReasonDetail style={{ width: "50%" }}>기숙사 생활을 위한 봉사활동</S.ReasonDetail>
+        <S.Divide />
+        <S.ReasonDetail style={{ width: "20%" }}>2022-09-21</S.ReasonDetail>
+        <S.Divide />
+        <S.ReasonDetail style={{ width: "10%" }}>윤성원</S.ReasonDetail>
+      </S.PointReasonDetailWrap>
+      <S.PointReasonDetailWrap>
+        <S.ReasonDetail style={{ width: "25%" }}>1점</S.ReasonDetail>
+        <S.Divide />
+        <S.ReasonDetail style={{ width: "50%" }}>기숙사 생활을 위한 봉사활동</S.ReasonDetail>
+        <S.Divide />
+        <S.ReasonDetail style={{ width: "20%" }}>2022-09-21</S.ReasonDetail>
+        <S.Divide />
+        <S.ReasonDetail style={{ width: "10%" }}>윤성원</S.ReasonDetail>
+      </S.PointReasonDetailWrap>
+      <S.PointReasonDetailWrap>
+        <S.ReasonDetail style={{ width: "25%" }}>1점</S.ReasonDetail>
+        <S.Divide />
+        <S.ReasonDetail style={{ width: "50%" }}>기숙사 생활을 위한 봉사활동</S.ReasonDetail>
+        <S.Divide />
+        <S.ReasonDetail style={{ width: "20%" }}>2022-09-21</S.ReasonDetail>
+        <S.Divide />
+        <S.ReasonDetail style={{ width: "10%" }}>윤성원</S.ReasonDetail>
+      </S.PointReasonDetailWrap>
+      <S.PointReasonDetailWrap>
+        <S.ReasonDetail style={{ width: "25%" }}>1점</S.ReasonDetail>
+        <S.Divide />
+        <S.ReasonDetail style={{ width: "50%" }}>기숙사 생활을 위한 봉사활동</S.ReasonDetail>
+        <S.Divide />
+        <S.ReasonDetail style={{ width: "20%" }}>2022-09-21</S.ReasonDetail>
+        <S.Divide />
+        <S.ReasonDetail style={{ width: "10%" }}>윤성원</S.ReasonDetail>
+      </S.PointReasonDetailWrap>
+      <S.PointReasonDetailWrap>
+        <S.ReasonDetail style={{ width: "25%" }}>1점</S.ReasonDetail>
+        <S.Divide />
+        <S.ReasonDetail style={{ width: "50%" }}>기숙사 생활을 위한 봉사활동</S.ReasonDetail>
+        <S.Divide />
+        <S.ReasonDetail style={{ width: "20%" }}>2022-09-21</S.ReasonDetail>
+        <S.Divide />
+        <S.ReasonDetail style={{ width: "10%" }}>윤성원</S.ReasonDetail>
+      </S.PointReasonDetailWrap>
+      <S.PointReasonDetailWrap>
+        <S.ReasonDetail style={{ width: "25%" }}>1점</S.ReasonDetail>
+        <S.Divide />
+        <S.ReasonDetail style={{ width: "50%" }}>기숙사 생활을 위한 봉사활동</S.ReasonDetail>
+        <S.Divide />
+        <S.ReasonDetail style={{ width: "20%" }}>2022-09-21</S.ReasonDetail>
+        <S.Divide />
+        <S.ReasonDetail style={{ width: "10%" }}>윤성원</S.ReasonDetail>
+      </S.PointReasonDetailWrap>
+      <S.PointReasonDetailWrap>
+        <S.ReasonDetail style={{ width: "25%" }}>1점</S.ReasonDetail>
+        <S.Divide />
+        <S.ReasonDetail style={{ width: "50%" }}>기숙사 생활을 위한 봉사활동</S.ReasonDetail>
+        <S.Divide />
+        <S.ReasonDetail style={{ width: "20%" }}>2022-09-21</S.ReasonDetail>
+        <S.Divide />
+        <S.ReasonDetail style={{ width: "10%" }}>윤성원</S.ReasonDetail>
+      </S.PointReasonDetailWrap>
 
     </S.DormitoryPointContainer>
   );
