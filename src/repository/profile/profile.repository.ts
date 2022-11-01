842d246001f3b1779323f815dcce22ce6dfafb70
임동현 <79711744+ldh3907@users.noreply.github.com>
2022-11-01 21:38:38 +0900

    🔀 ::비밀번호 변경 로직 추가

--- src/repository/profile/profile.repository.ts
+++ src/repository/profile/profile.repository.ts
@@ -1,10 +1,18 @@
 import { customAxios } from "../../lib/axios/customAxios";
 import { myProfileResponse } from "../../types/profile/profile.type";
+import { patchMyPasswordParam } from "./profile.param";
 
 class ProfileRepository {
   public async getMyProfileInfo(): Promise<myProfileResponse> {
     const { data } = await customAxios.get("/members/my");
     return data;
+  }
+
+  public async patchMyPassword({
+    newPw,
+    pw,
+  }: patchMyPasswordParam): Promise<void> {
+    await customAxios.patch("/members/pw", { newPw, pw });
   }
 }
 
