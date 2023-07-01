e13413b5d6d794364041431b530bff854d7fd0e5
박상현 <104070112+Sanghyun0505@users.noreply.github.com>
2023-07-01 11:19:40 +0900

    ♻️ :: 로그아웃 버튼 포지션속성으로 인한 디자인 수정

--- src/components/MyInfo/Contents/Nav/Logout/style.ts
+++ src/components/MyInfo/Contents/Nav/Logout/style.ts
@@ -2,8 +2,6 @@ import styled from "styled-components";
 import { palette } from "../../../../../styles/palette";
 
 export const LogoutWrap = styled.div`
-  position: absolute;
-  bottom: 130px;
   width: 130px;
   text-align: center;
   padding: 15px 0;
@@ -13,6 +11,7 @@ export const LogoutWrap = styled.div`
   /* margin-top: 30px;
   width: calc(100% - 60px - 100px);
   display: flex; */
+  margin-top: 50px;
   align-items: center;
   justify-content: center;
   font-size: 1.3rem;
