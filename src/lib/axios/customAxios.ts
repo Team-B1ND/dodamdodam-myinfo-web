51ce56000a9785235a155dd514034f806d6cac3a
박상현 <104070112+Sanghyun0505@users.noreply.github.com>
2024-03-10 13:05:40 +0900

    feat: test axios 생성

--- src/lib/axios/customAxios.ts
+++ src/lib/axios/customAxios.ts
@@ -1,14 +1,25 @@
-import axios, { AxiosError, AxiosRequestConfig, AxiosResponse } from "axios";
-import { REQUEST_TOKEN_KEY } from "../../constants/token/token.constant";
+import axios from "axios";
+import {
+  ACCESS_TOKEN_KEY,
+  REQUEST_TOKEN_KEY,
+} from "../../constants/token/token.constant";
 import config from "../../config/config.json";
-import cookie from "../cookie/cookie";
 import { customAxiosErrorInterceptor } from "./interceptor";
+import token from "../token/token";
 
 export const customAxios = axios.create({
   baseURL: config.DODAM_SERVER_V6,
   headers: {
     "Access-Control-Allow-Origin": "*",
-    [REQUEST_TOKEN_KEY]: `Bearer ${cookie.getCookie("access-token")}`,
+    [REQUEST_TOKEN_KEY]: `Bearer ${token.getToken(ACCESS_TOKEN_KEY)}`,
+  },
+});
+
+export const testAxios = axios.create({
+  baseURL: config.DODAM_TEST_SERVER,
+  headers: {
+    "Access-Control-Allow-Origin": "*",
+    [REQUEST_TOKEN_KEY]: `Bearer ${token.getToken(ACCESS_TOKEN_KEY)}`,
   },
 });
 
@@ -16,3 +27,5 @@ customAxios.interceptors.response.use(
   (res) => res,
   customAxiosErrorInterceptor
 );
+
+testAxios.interceptors.response.use((res) => res, customAxiosErrorInterceptor);
